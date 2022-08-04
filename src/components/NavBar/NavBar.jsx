cd45da11e117872cefa0559a48fe40703fc43d73
lasha tatulashvili <lashatatu@gmail.com>
2022-08-04 17:25:42 +0400

    fl pire section 4 2 sidebar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,78 @@
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Drawer,
+  Button,
+  Avatar,
+  useMediaQuery,
+} from "@mui/material";
+import {
+  Menu,
+  AccountCircle,
+  Brightness4,
+  Brightness7,
+} from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import { useTheme } from "@mui/material/styles";
+
+import useStyles from "./styles";
+
 const NavBar = () => {
-  console.log('navbar');
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(max-width:600px)");
+  const theme = useTheme();
+  const isAuthenticated = true;
   return (
-    <div>
-      <p>NavBar</p>
-    </div>
+    <>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}>
+          {isMobile && (
+            <IconButton
+              color={"inherit"}
+              edge={"start"}
+              style={{ outline: "none" }}
+              onClick={() => {}}
+              className={classes.menuButton}
+            >
+              <Menu />
+            </IconButton>
+          )}
+          <IconButton color={"inherit"} sx={{ ml: 1 }} onClick={() => {}}>
+            {(theme.palette.mode = "dark" ? <Brightness7 /> : <Brightness4 />)}
+          </IconButton>
+          {!isMobile && "Search ..."}
+          <div>
+            {!isAuthenticated ? (
+              <Button color={"inherit"} onClick={() => {}}>
+                Login &nbsp; <AccountCircle />
+              </Button>
+            ) : (
+              <Button
+                color={"inherit"}
+                component={Link}
+                to={`/profile/:id`}
+                className={classes.linkButton}
+                onClick={() => {}}
+              >
+                {!isMobile && <>My Movies &nbsp;</>}
+                <Avatar
+                  style={{
+                    width: 30,
+                    height: 30,
+                  }}
+                  alt={"Profile"}
+                  src={
+                    "https://thumbs.dreamstime.com/b/default-avatar-photo-placeholder-profile-icon-eps-file-easy-to-edit-default-avatar-photo-placeholder-profile-icon-124557887.jpg"
+                  }
+                />
+              </Button>
+            )}
+          </div>
+          {isMobile && "Search ..."}
+        </Toolbar>
+      </AppBar>
+    </>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDom from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-import App from './components/App';
+import App from "./components/App";
+
+const theme = createTheme({});
 
 ReactDom.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root'),
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
+  document.getElementById("root")
 );
-
