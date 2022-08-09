9ba53b23418bd876fc3f7d68cf4a6af36bacd7dd
lasha tatulashvili <lashatatu@gmail.com>
2022-08-09 15:51:49 +0400

    fl pire section 4 14 movie information part 1

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,12 +1,35 @@
 import { useSelector } from "react-redux";
 import { userSelector } from "../../features/auth";
+import { Box, Button, Typography } from "@mui/material";
+import { ExitToApp } from "@mui/icons-material";
 
 const Profile = () => {
   const { user } = useSelector(userSelector);
+
+  const favoriteMovies = [];
+  const logout = () => {
+    localStorage.clear();
+    window.location.href = "/";
+  };
   return (
-    <div>
-      <p>{user.username}</p>
-    </div>
+    <Box>
+      <Box display={"flex"} justifyContent={"space-between"}>
+        <Typography variant={"h4"} gutterBottom>
+          My profile
+        </Typography>
+        <Button color={"inherit"} onClick={logout}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+
+      {!favoriteMovies.length ? (
+        <Typography variant={"h5"}>
+          Add favorites or watchlist some movies to see them here
+        </Typography>
+      ) : (
+        <Box>Favorite Movies</Box>
+      )}
+    </Box>
   );
 };
 
