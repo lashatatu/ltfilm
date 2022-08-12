7e34c60a152e220aad78699ef6c6945790440ea5
lasha tatulashvili <lashatatu@gmail.com>
2022-08-12 11:27:05 +0400

    fl pire section 4 22. Responsiveness Adjustments

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -21,6 +21,9 @@ const Movies = () => {
     searchQuery,
   });
 
+  const lg = useMediaQuery((theme) => theme.breakpoints.only("lg"));
+  const numberOfMovies = lg ? 16 : 18;
+
   if (isFetching) {
     return (
       <Box display={"flex"} justifyContent={"center"}>
@@ -47,7 +50,7 @@ const Movies = () => {
 
   return (
     <div>
-      <MovieList movies={data} />
+      <MovieList movies={data} numberOfMovies={numberOfMovies} />
       <Pagination
         currentPage={page}
         setPage={setPage}
